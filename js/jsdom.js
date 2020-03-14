0f5d081793ad452edb314e70f95a1b96c6f998e7
thebuildhub <joshi.mohit1221@gmail.com>
2020-03-14 09:58:04 +0530

    old file submittion

--- js/jsdom.js
+++ js/jsdom.js
@@ -4,20 +4,20 @@ console.log(document.querySelectorAll('.items'));
 
 
 
-const btn = document.querySelector('.btn');
-btn.addEventListener('click',btnClick);
-
-function btnClick(e){
-  e.preventDefault();
-  document.querySelector('section').style.background = 'red';
-}
-
-btn.addEventListener('focusout',btnFocus);
-
-function btnFocus(e){
-  e.preventDefault();
-  document.querySelector('section').style.background = '#fff';
-}
+// const btn = document.querySelector('.btn');
+// btn.addEventListener('click',btnClick);
+//
+// function btnClick(e){
+//   e.preventDefault();
+//   document.querySelector('section').style.background = 'red';
+// }
+//
+// btn.addEventListener('focusout',btnFocus);
+//
+// function btnFocus(e){
+//   e.preventDefault();
+//   document.querySelector('section').style.background = '#fff';
+// }
 
 const menubtn = document.querySelector('.menu-bar');
 
@@ -33,4 +33,19 @@ menubtn.addEventListener('focusout',menuclose);
 function menuclose(e){
   document.querySelector('.menu-show').classList.add('menu');
   document.querySelector('.menu').classList.remove('menu-show');
+}
+
+//form validation and list creation
+const myform = document.querySelector("#my-form");
+const name = document.querySelector("#name");
+const email = document.querySelector("#email");
+const msg = document.querySelector(".msg");
+const userlist = document.querySelector(".items");
+
+myform.addEventListener('submit',onsubmit);
+
+function onsubmit(e){
+  e.preventDefault();
+  console.log(name.value);
+  console.log('hello here')
 }
